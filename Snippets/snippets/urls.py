b98953e276fb1cb956cf3aeb539f1e650f2c66fb
Islam Mesha <eng.islammesha@gmail.com>
2018-12-12 21:57:50 +0200

    Manage our urls using routers

--- Snippets/snippets/urls.py
+++ Snippets/snippets/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.schemas import get_schema_view
 from snippets import views
@@ -15,16 +16,29 @@ from snippets.views import UserViewSet, SnippetViewSet
 #     path('snippets/<int:pk>', SnippetDetail.as_view(), name='snippet-detail')
 # ]
 
+# urlpatterns = [
+#     path('', views.api_root, name='api_root'),
+#     path('schema/', get_schema_view(title='Pastebin API'), name='schema_view'),
+#     path('users/', UserViewSet.as_view({'get': 'list'}), name='user-list'),
+#     path('users/<int:pk>', UserViewSet.as_view({'get': 'retrieve'}), name='user-detail'),
+#     path('snippets/', SnippetViewSet.as_view({'get': 'list', 'post': 'create'}), name='snippet-list'),
+#     path('snippets/<int:pk>',
+#          SnippetViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}),
+#          name='snippet-detail'),
+#     path('snippets/<int:pk>/highlight/', SnippetViewSet.as_view({'get': 'highlight'}), name='snippet-highlight')
+# ]
+
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'snippets', views.SnippetViewSet)
+
+# The API URLs are now determined automatically by the router.
+
 urlpatterns = [
-    path('', views.api_root, name='api_root'),
+    path('', include(router.urls)),
     path('schema/', get_schema_view(title='Pastebin API'), name='schema_view'),
-    path('users/', UserViewSet.as_view({'get': 'list'}), name='user-list'),
-    path('users/<int:pk>', UserViewSet.as_view({'get': 'retrieve'}), name='user-detail'),
-    path('snippets/', SnippetViewSet.as_view({'get': 'list', 'post': 'create'}), name='snippet-list'),
-    path('snippets/<int:pk>',
-         SnippetViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}),
-         name='snippet-detail'),
-    path('snippets/<int:pk>/highlight/', SnippetViewSet.as_view({'get': 'highlight'}), name='snippet-highlight')
 ]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+# urlpatterns = format_suffix_patterns(urlpatterns)
